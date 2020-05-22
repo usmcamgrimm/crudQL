4eb409d4c67b1798815093074a1b580897caf177
Adrian Grimm <usmcamgrimm@gmail.com>
2020-05-22 18:58:29 -0400

    Starting over

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
 // Import the required packages
-const mysql = require('mysql')
-const express = require('express')
-const bodyparser = require('body-parser')
-let app = express()
-
-// Configure express server
-app.use(bodyparser.json())
+let mysql = require('mysql');
+let express = require('express');
 
 require('dotenv').config();
 
 // Establish connection with the mysql database
 // MySQL connection details
-let mysqlConnection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    multipleStatements: true
-})
+    database: process.env.DATABASE
+});
 
-// On connection success/fail:
-mysqlConnection.connect(err => {
-    if (!err) console.log('Connection Successful')
-    else console.log('Connection failed' + JSON.stringify(err, undefined, 2))
-})
+let app = express();
 
-//Set server connect and port variable
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Listening on port ${port}..`))
+connection.connect(function(err) {
+    if(!err) {
+        console.log('Database is connected');
+    } else {
+        console.log("Error connecting to the database");
+    }
+});
 
-//Create GET router to fetch data from mysql
-app.get('/learners', (req, res) => {
-    mysqlConnection.query('SELECT * FROM learnerdetails', (err, rows, fields) => {
-        if (!err)
-            res.send(rows);
-        else
-            console.log(err);
+app.get('/witcher', function(req,res) {
+    connection.query('SELECT * FROM books', function(err, rows, fields) {
+        connection.end();
+            if (!err)
+                console.log('The solution is: ', rows);
+            else
+                console.log('Error while performing query.');
     })
 })
 
-// Query DB for specific learner details
-app.get('/learners/:id', (req, res) => {
-    mysqlConnection.query('SELECT * FROM learnerdetails WHERE learner_id = ?', [req.params.id], (err, rows, fields) => {
-        if (!err) res.send(rows);
-        else
-            console.log(err);
-    })
-})
+app.listen(8080);
