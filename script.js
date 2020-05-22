65e23ce53c608945a125a1ceab033a2e52796968
Adrian Grimm <usmcamgrimm@gmail.com>
2020-05-22 14:18:20 -0400

    Update for .env

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 // Import the required packages
-const mysql = require('mysql');
-const express = require('express');
-const bodyparser = require('body-parser');
-let app = express();
+const mysql = require('mysql')
+const express = require('express')
+const bodyparser = require('body-parser')
+let app = express()
 
 // Configure express server
-app.use(bodyparser.json());
+app.use(bodyparser.json())
+
+require('dotenv').config();
 
 // Establish connection with the mysql database
 // MySQL connection details
 let mysqlConnection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     multipleStatements: true
-});
+})
 
 // On connection success/fail:
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('Connection Successful');
-    else
-        console.log('Connection failed' + JSON.stringify(err, undefined, 2));
-});
+mysqlConnection.connect(err => {
+    if (!err) console.log('Connection Successful')
+    else console.log('Connection failed' + JSON.stringify(err, undefined, 2))
+})
 
 //Set server connect and port variable
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
-
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}..`))
