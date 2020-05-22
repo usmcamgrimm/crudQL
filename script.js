90c26b84386db08142fbd38a49ff9e22bf1aadb6
Adrian Grimm <usmcamgrimm@gmail.com>
2020-05-22 10:22:05 -0400

    Add script.js
    Also added missing dependencies:
    nodemon and mysql

---
+++ script.js
@@ -0,0 +1,30 @@
+// Import the required packages
+const mysql = require('mysql');
+const express = require('express');
+const bodyparser = require('body-parser');
+let app = express();
+
+// Configure express server
+app.use(bodyparser.json());
+
+// Establish connection with the mysql database
+// MySQL connection details
+let mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '**************',
+    database: 'learners',
+    multipleStatements: true
+});
+
+// On connection success/fail:
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('Connection Successful');
+    else
+        console.log('Connection failed' + JSON.stringify(err, undefined, 2));
+});
+
+//Set server connect and port variable
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
