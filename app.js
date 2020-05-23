4285cc967a51d22cd2dfdd2fc40698183b9eca1d
Adrian Grimm <usmcamgrimm@gmail.com>
2020-05-23 17:45:03 -0400

    Fuck I give up
    Fixed a bunch of errors but I can't want to think about this anymore right now

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const TodoDao = require('./TodoDao.js');
-const TodoDao = new TodoDao();
+const todoDao = new TodoDao();
 
 const app = async () => {
-    let savedTodo = await TodoDao.saveEntrity({
+    let savedTodo = await todoDao.saveEntity({
         title: "Figure out how to use MySQL with NodeLS",
         completed: 0
     });
     console.log("saved Todo --> ".savedTodo);
 
     savedTodo.completed = 1;
-    let isUpdated = await TodoDao.updateEntity(savedTodo);
+    let isUpdated = await todoDao.updateEntity(savedTodo);
     console.log("Is it updated --> ", isUpdated);
 
-    let todoList = await TodoDao.readEntities();
+    let todoList = await todoDao.readEntities();
     console.log("List of todo --> ", todoList);
 
-    let isDeleted = await TodoDao.deleteEntity(savedTodo.id);
+    let isDeleted = await todoDao.deleteEntity(savedTodo.id);
     console.log("Is it deleted --> ", isDeleted);
 }
 
